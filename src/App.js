d0959266696dd20612a9b1a3f9b82dbd0be6089d
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2021-02-24 21:04:20 +0100

    implement react router

--- src/App.js
+++ src/App.js
@@ -1,82 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import './App.css';
-import Movie from './components/Movie';
-import { useDispatch } from 'react-redux'
-
-const apiKey = process.env.REACT_APP_API_KEY;
-const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=1`;
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
-
+import HomePage from './pages/HomePage';
+import { Route } from 'react-router-dom';
 
 function App() {
-  const dispatch = useDispatch();
-  const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const getMovies = (API) => {
-    fetch(API)
-      .then((res) => res.json())
-      .then((data) => {
-        setMovies(data.results);
-        console.log(data)
-      })
-  }
-
-  const fetchMovies = (API) => (dispatch) => {
-    const popularData = fetch(API)
-      .then((res) => res.json())
-      .then((data) => {
-        // setMovies(data.results);
-        dispatch({
-          type: 'FETCH_MOVIES',
-          payload: {
-            popular: data.results,
-          }
-        })
-      })
-
-  }
-
-  useEffect(() => {
-    getMovies(FEATURED_API);
-    dispatch(fetchMovies(FEATURED_API))
-  }, []);
-
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    if (searchTerm) {
-      getMovies(SEARCH_API + searchTerm)
-      setSearchTerm('')
-    }
-  }
-
-  const handleOnChange = (e) => {
-    setSearchTerm(e.target.value);
-  }
-
   return (
     <>
-      <header>
-        <h1 className='title'>Moovies</h1>
-        <form onSubmit={handleOnSubmit}>
-          <input
-            className='search'
-            type="text"
-            placeholder='Search...'
-            value={searchTerm}
-            onChange={handleOnChange} />
-        </form>
-      </header>
-
-      <div className="movie-container">
-        {
-          movies.length > 0 && movies.map((movie) =>
-            < Movie
-              key={movie.id}
-              {...movie} />
-          )
-        }
-      </div>
+      <Route path={['/movie/:id', '/']}>
+        <HomePage />
+      </Route>
     </>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const IMG_API = `https://image.tmdb.org/t/p/w1280`;
 
@@ -14,31 +16,33 @@ const setVoteClass = (vote) => {
     }
 }
 
-const Movie = ({ title, poster_path, overview, vote_average }) => {
+const Movie = ({ title, poster_path, overview, vote_average, id }) => {
     return (
-        <div className='movie'>
-            <img src={poster_path ? IMG_API + poster_path : 'https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80'} alt={title} />
-            <div className="movie-info">
-                <h3>{title}</h3>
-                <span
-                    className={`badge ${setVoteClass(vote_average)}`}
-                >{vote_average}
-                </span>
-            </div>
-            <div className="movie-overview">
-                <div className="overview-title">
+        <Link to={`/movie/${id}`}>
+            <div className='movie'>
+                <img src={poster_path ? IMG_API + poster_path : 'https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80'} alt={title} />
+                <div className="movie-info">
                     <h3>{title}</h3>
                     <span
                         className={`badge ${setVoteClass(vote_average)}`}
                     >{vote_average}
                     </span>
                 </div>
-                <div className="overview-text">
-                    <h4>Overview:</h4>
-                    <p >{overview} {overview}</p>
+                <div className="movie-overview">
+                    <div className="overview-title">
+                        <h3>{title}</h3>
+                        <span
+                            className={`badge ${setVoteClass(vote_average)}`}
+                        >{vote_average}
+                        </span>
+                    </div>
+                    <div className="overview-text">
+                        <h4>Overview:</h4>
+                        <p >{overview} {overview}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
